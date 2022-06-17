81e72285ec276680200613df4683c718cc044ea0
Ing. Daniel Torres Ruiz <torresruizdaniel23@gmail.com>
2022-06-16 23:08:34 -0500

    TK-004: Grouping the exception thown in query layer
    TK-004: Grouping the exception thown in query layer

--- src/main/java/com/dtorres/api/pokedex/query/infrastructure/exception/helper/QueryExceptionHelper.java
+++ src/main/java/com/dtorres/api/pokedex/query/infrastructure/exception/helper/QueryExceptionHelper.java
@@ -1,7 +1,7 @@
 package com.dtorres.api.pokedex.query.infrastructure.exception.helper;
 
-import com.dtorres.api.pokedex.commons.domain.exception.DominioException;
-import com.dtorres.api.pokedex.commons.domain.exception.ErrorTecnicoException;
+import com.dtorres.api.pokedex.commons.domain.exception.DomainException;
+import com.dtorres.api.pokedex.commons.domain.exception.TechnicalException;
 import com.dtorres.api.pokedex.commons.helper.ThrowExceptionHelper;
 import com.dtorres.api.pokedex.commons.log.LogErrorManager;
 
@@ -11,16 +11,16 @@ public class QueryExceptionHelper extends ThrowExceptionHelper {
 
     private static final String DEFAULT_MESSAGE = "Not enough information";
 
-    private static ErrorTecnicoException getErrorTecnicoException(Throwable throwable) {
+    private static TechnicalException getErrorTecnicoException(Throwable throwable) {
         LogErrorManager.error(DEFAULT_MESSAGE, throwable);
 
-        if (throwable.getCause() != null && throwable.getCause() instanceof DominioException) {
-            return new ErrorTecnicoException(throwable.getCause().getMessage());
+        if (throwable.getCause() != null && throwable.getCause() instanceof DomainException) {
+            return new TechnicalException(throwable.getCause().getMessage());
         }
-        if (throwable instanceof DominioException) {
-            return new ErrorTecnicoException(throwable.getCause().getMessage());
+        if (throwable instanceof DomainException) {
+            return new TechnicalException(throwable.getCause().getMessage());
         }
-        return new ErrorTecnicoException(DEFAULT_MESSAGE);
+        return new TechnicalException(DEFAULT_MESSAGE);
     }
 
     public static <T> T throwObject(Throwable throwable) {
